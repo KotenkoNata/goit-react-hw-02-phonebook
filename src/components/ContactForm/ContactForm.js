da9af2d6c295d3845319894ac53a19e4b9e631b5
KotenkoNata <kotenkonata8@gmail.com>
2021-03-11 19:52:34 +0200

    add proptypes

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+
 import styles from './ContactForm.module.css';
 
 class ContactForm extends Component {
   state = {
     name: '',
     number: '',
+  };
+
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
   };
 
   handleChange = e => {
